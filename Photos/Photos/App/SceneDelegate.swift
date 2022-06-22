7cd269d02b784cb1c4a565432c363aee92e81efe
meregrey <yeojinyoon.dev@gmail.com>
2022-06-22 22:12:41 +0900

    프로젝트 생성

---
+++ Photos/Photos/App/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  Photos
+//
+//  Created by Yeojin Yoon on 2022/06/22.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
