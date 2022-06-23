39c025dd421e8deeeafff9fbf21f910b5aead168
meregrey <yeojinyoon.dev@gmail.com>
2022-06-23 13:57:23 +0900

    데이터 로드를 담당하는 DataLoader 구조체 정의

---
+++ Photos/Photos/Networking/DataLoader.swift
@@ -0,0 +1,38 @@
+//
+//  DataLoader.swift
+//  Photos
+//
+//  Created by Yeojin Yoon on 2022/06/23.
+//
+
+import Foundation
+
+struct DataLoader: DataLoadable {
+    let urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+    
+    func fetch<T: Decodable>(with endpoint: Endpoint, completion: @escaping (Result<T, LoadingError>) -> Void) {
+        guard let request = endpoint.makeRequest() else { return }
+        
+        let task = urlSession.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.requestFailed(error: error!)))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+                completion(.failure(.decodingFailed))
+                return
+            }
+            completion(.success(result))
+        }
+        
+        task.resume()
+    }
+}
