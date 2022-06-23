f15f9878b5c91e3a56fcda42790249fe0157bb2d
meregrey <yeojinyoon.dev@gmail.com>
2022-06-23 18:30:32 +0900

    LoadingError에 이미지 로드 관련 케이스 추가

--- Photos/Photos/Networking/LoadingError.swift
+++ Photos/Photos/Networking/LoadingError.swift
@@ -11,12 +11,21 @@ enum LoadingError: Error {
     case requestFailed(error: Error)
     case noData
     case decodingFailed
+    case invalidURL
+    case invalidImageData
     
     var message: String {
         switch self {
-        case .requestFailed(let error): return error.localizedDescription
-        case .noData: return "데이터가 존재하지 않습니다."
-        case .decodingFailed: return "디코딩하던 중 에러가 발생했습니다."
+        case .requestFailed(let error):
+            return error.localizedDescription
+        case .noData:
+            return "데이터가 존재하지 않습니다."
+        case .decodingFailed:
+            return "디코딩하던 중 에러가 발생했습니다."
+        case .invalidURL:
+            return "URL이 유효하지 않습니다."
+        case .invalidImageData:
+            return "이미지 데이터가 유효하지 않습니다."
         }
     }
 }
