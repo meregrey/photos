12feaae101abcc204b040553e82afb525ecedcc9
meregrey <yeojinyoon.dev@gmail.com>
2022-07-07 17:37:44 +0900

    LoadingError의 case 및 description 수정

--- Photos/Photos/Networking/LoadingError.swift
+++ Photos/Photos/Networking/LoadingError.swift
@@ -8,24 +8,21 @@
 import Foundation
 
 enum LoadingError: Error {
-    case requestFailed(error: Error)
-    case noData
-    case decodingFailed
-    case invalidURL
     case invalidImageData
+    case invalidRequest
+    case invalidURL
+    case requestFailed
     
-    var message: String {
+    var localizedDescription: String {
         switch self {
-        case .requestFailed(let error):
-            return error.localizedDescription
-        case .noData:
-            return "데이터가 존재하지 않습니다."
-        case .decodingFailed:
-            return "디코딩하던 중 에러가 발생했습니다."
-        case .invalidURL:
-            return "URL이 유효하지 않습니다."
         case .invalidImageData:
             return "이미지 데이터가 유효하지 않습니다."
+        case .invalidRequest:
+            return "요청이 유효하지 않습니다."
+        case .invalidURL:
+            return "URL이 유효하지 않습니다."
+        case .requestFailed:
+            return "요청이 실패했습니다."
         }
     }
 }
