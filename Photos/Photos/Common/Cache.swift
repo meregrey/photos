6f74a52fb39128931cbd9affd1fb06fb1b2cd119
meregrey <yeojinyoon.dev@gmail.com>
2022-09-14 10:20:57 +0900

    Cache의 불필요한 NSLock 제거
    - NSCache가 자체적으로 액세스를 관리하므로 직접 잠글 필요가 없음

--- Photos/Photos/Common/Cache.swift
+++ Photos/Photos/Common/Cache.swift
@@ -9,34 +9,21 @@ import Foundation
 
 final class Cache<Key: Hashable, Value> {
     private let wrapped = NSCache<WrappedKey, WrappedValue>()
-    private let lock = NSLock()
     
     func value(for key: Key) -> Value? {
-        lock.lock()
-        defer { lock.unlock() }
-        
         let wrappedValue = wrapped.object(forKey: WrappedKey(key))
         return wrappedValue?.value
     }
     
     func insert(_ value: Value, for key: Key) {
-        lock.lock()
-        defer { lock.unlock() }
-        
         wrapped.setObject(WrappedValue(value), forKey: WrappedKey(key))
     }
     
     func removeValue(for key: Key) {
-        lock.lock()
-        defer { lock.unlock() }
-        
         wrapped.removeObject(forKey: WrappedKey(key))
     }
     
     func removeAllValues() {
-        lock.lock()
-        defer { lock.unlock() }
-        
         wrapped.removeAllObjects()
     }
     
